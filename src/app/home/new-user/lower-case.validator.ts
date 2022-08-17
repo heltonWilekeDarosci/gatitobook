3e9b31f09411d97b1160597c53a61d77db061664
helton <heltoloco@hotmail.com>
2022-08-17 18:36:17 -0300

    Create validator for useName

---
+++ src/app/home/new-user/lower-case.validator.ts
@@ -0,0 +1,10 @@
+import { AbstractControl } from "@angular/forms";
+
+export function lowerCaseValidator(control: AbstractControl) {
+  const value = control.value as string
+  if (value !== value.toLocaleLowerCase()) {
+    return { lowerCase: true };
+  } else {
+    return null;
+  }
+}
