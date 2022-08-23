7ac5a4cd53de1d957461b23a140daf0ae625b9d8
helton <heltoloco@hotmail.com>
2022-08-23 15:36:54 -0300

    Create reusable card component for animals

---
+++ src/app/animals/animal/animal.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+const API = 'http://localhost:3000';
+
+@Component({
+  selector: 'app-animal',
+  templateUrl: './animal.component.html',
+  styleUrls: ['./animal.component.css']
+})
+export class AnimalComponent implements OnInit {
+  private originalUrl = '';
+
+  @Input() descricao = '';
+
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
+      this.originalUrl = url;
+    } else {
+      this.originalUrl = `${API}/imgs/${url}`;
+    }
+  }
+
+  get url(): string {
+    return this.originalUrl;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
