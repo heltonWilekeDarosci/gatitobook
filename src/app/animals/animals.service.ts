f83ca5137a337cc634c7d9685b7c21d9b3347739
helton <heltoloco@hotmail.com>
2022-08-23 15:52:44 -0300

    Create animal service and interface

---
+++ src/app/animals/animals.service.ts
@@ -0,0 +1,23 @@
+import { TokenService } from './../authentication/token.service';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { animals } from './animals';
+
+const API = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimalsService {
+
+  constructor(private http: HttpClient, private tokenService: TokenService) { }
+
+  userList(userName: string): Observable<animals> {
+    const token = this.tokenService.returnToken();
+    const headers = new HttpHeaders().append('x-access-token', token);
+    return this.http.get<animals>(`${API}/${userName}/photos`, {
+      headers,
+    });
+  }
+}
